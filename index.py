f508cf71e67e2c4ff2c67085eba4c4a0eb30c197
ZapTap <camerondickins@gmail.com>
2023-03-17 15:38:39 -0400

    remove main2()

--- index.py
+++ index.py
@@ -22,53 +22,5 @@ def main():
 
 	return
 
-def main2():
-	# Load settings
-	# if not os.path.exists(os.path.join(os.getcwd(), SETTINGSFILENAME)):
-	# 	print('Unable to find settings file .\\\'' + SETTINGSFILENAME + '\'')
-	# 	return -1
-	# with open(SETTINGSFILENAME, 'r') as file:
-	# 	settings = json.load(file)
-
-
-	# Initialize pygame
-	pygame.init()
-	clock = pygame.time.Clock()
-	canvas = pygame.display.set_mode(settings['Env']['Resolution'])
-	pygame.display.set_caption(settings['Env']['Name'])
-
-	# frameCounter = FPSCounter(canvas, clock, settings['Obj']['FPS'])
-	player = Snake(canvas, settings)
-
-	player.move(food = True)
-	player.move(food = True)
-	player.move(food = True)
-	player.move(food = True)
-	player.move(food = True)
-	player.move(food = True)
-
-	state = 1
-	while state > 0:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				state = 0
-			if event.type == pygame.KEYDOWN:
-				if event.key in settings['Obj']['Player']['Keys']:
-					player.input(event.key)
-				if event.key == pygame.K_ESCAPE: state = 0
-			if event.type == player.refresh:
-				player.move()
-
-
-		clock.tick(settings['Env']['FPS'])
-		canvas.fill(settings['Env']['BackgroundColor'])
-
-		player.draw()
-		# frameCounter.draw()
-
-		pygame.display.update()
-
-	return
-
 if __name__ == '__main__':
 	main()
